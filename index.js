fb8080240dcd3a0d90fedd9c2594e68bbce1d7d8
sivaprasath2004 <prasathsiva2004@gmail.com>
2024-04-03 17:20:05 +0530

    bug

--- index.js
+++ index.js
@@ -1,43 +1,54 @@
-const express=require('express')
-const app=express()
-const http=require('http')
-const socketio=require('socket.io')
-const server=http.createServer(app)
-const io=socketio(server,{cors:{origin:'*'}})
-const {addUsers,removeUser,getUser}=require('./entity')
-const App=require('./app')
-app.get('/',(req,res)=>res.send("worked"))
-io.on("connect",socket=>{
-  console.log('Connect')
-  socket.on('join', ({name,room},callBack) => {
-    console.log('joined')
-    const {user,error}=addUsers({id:socket.id,name:name,room:room})
-    if(error){
-      callBack(error)
-      console.log(error)
+const express = require("express");
+const app = express();
+const http = require("http");
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server, { cors: { origin: "*" } });
+const cors = require("cors");
+const { addUsers, removeUser, getUser } = require("./entity");
+app.get("/", (req, res) => res.send("worked"));
+app.use(cors());
+io.on("connect", (socket) => {
+  console.log("Connect");
+  socket.on("join", ({ name, room }, callBack) => {
+    console.log("joined");
+    const { user, error } = addUsers({ id: socket.id, name: name, room: room });
+    if (error) {
+      callBack(error);
+      console.log(error);
       return;
     }
     socket.join(user.room);
-    socket.emit('message',{user:'admin',text:`welcome ${user.name} join the room`})
-    socket.broadcast.to(user.room).emit('message',{user:'admin',text:`${user.name} has join the room`})
+    socket.emit("message", {
+      user: "admin",
+      text: `welcome ${user.name} join the room`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", {
+        user: "admin",
+        text: `${user.name} has join the room`,
+      });
   });
-  socket.on('sendMes', (message,callBack) => {
-    console.log(message)
-    const user=getUser(socket.id)
-    if(user){
-    io.to(user.room).emit('message',{ user:user.name, text : message})
+  socket.on("sendMes", (message, callBack) => {
+    console.log(message);
+    const user = getUser(socket.id);
+    if (user) {
+      io.to(user.room).emit("message", { user: user.name, text: message });
+    } else {
+      callBack("user Not found");
     }
-    else{
-      callBack("user Not found")
-    }
-  })
-  socket.on('disconnect',()=>{
-    console.log("disconnect")
+  });
+  socket.on("disconnect", () => {
+    console.log("disconnect");
     const user = removeUser(socket.id);
-   console.log('users remove',user)
-    if(user) {  
-      io.to(user.room).emit('message',{ user: 'admin', text : `${user.name} left` })
+    console.log("users remove", user);
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "admin",
+        text: `${user.name} left`,
+      });
     }
-  })
-})
-server.listen(8000,()=>console.log("server is 8000"))
+  });
+});
+server.listen(8000, () => console.log("server is 8000"));
